b6cbd793fae49a64b2956f72815113ed9145b87c
Nikita choudhary <90494881+Nikita-choudhary09@users.noreply.github.com>
2025-05-06 00:37:10 +0530

    Time: 0 ms (100%), Space: 15 MB (21.19%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,14 +1,24 @@
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};
-                }
+        unordered_map<int, int> num_to_index;  // value -> index
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+            
+            // If complement is already in the map, return the pair
+            if (num_to_index.find(complement) != num_to_index.end()) {
+                return {num_to_index[complement], i};
             }
+            
+            // Store current number and its index
+            num_to_index[nums[i]] = i;
         }
-        return {}; // Just in case, though problem guarantees a solution
+        
+        return {}; // Shouldn't be reached (as per problem constraint)
     }
 };
